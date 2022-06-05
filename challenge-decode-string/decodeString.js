dc1eeaa42b42f1da2afcf24eceee05f696f16f74
Logan Leatherow <lleatherow@niagarawater.com>
2022-06-05 18:26:50 -0500

    decode string done

--- challenge-decode-string/decodeString.js
+++ challenge-decode-string/decodeString.js
@@ -1,1 +1,23 @@
 // Write your code below this line
+const decoder = string => {
+    let decipher = Number(string[0])
+    let decoded = ""
+    for (let i = 1; i < string.length; i++) {
+        let value = string[i].charCodeAt() + decipher
+        decoded += String.fromCharCode(value)
+}
+return decoded
+}
+
+console.log(decoder("2fcjjm"))
+
+
+
+
+
+
+
+
+// >>> "1a" // "b"
+// >>> "3ce" // "fh"
+// >>> "2fcjjm" // "hello"
